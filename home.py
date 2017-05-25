4429a7b9f0d452ca9b7f48269a07e4d62b34673d
Darren MacKenzie <darren277@yahoo.com>
2017-05-25 15:05:27 -0700

    Pre-deployment commit.

--- home.py
+++ home.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/zerotoapp')
+def zerotoapp():
+    return render_template("zerotoapp.html")
+
 if __name__ == "__main__":
     app.run()
