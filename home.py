9be2655225fee723d0e3614c674496f6c18c87cc
Darren MacKenzie <darren277@yahoo.com>
2017-06-07 18:29:33 -0700

    Pre-deployment commit.

--- home.py
+++ home.py
@@ -7,19 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route('/zerotoapp')
-def zerotoapp():
-    return render_template("zerotoapp.html")
+@app.route('/aboutme')
+def aboutme():
+    return render_template("aboutme.html")
 
 @app.route('/stickynotes')
 def stickynotes():
     return render_template("stickynotes.html")
 
-@app.route('/kanban')
-def kanban():
-    return render_template("kanban.html")
+@app.route('/evergraph')
+def evergraph():
+    return render_template("evergraph.html")
+
+@app.route('/css')
+def css():
+    return render_template("cssstuff.html")
 
 if __name__ == "__main__":
     app.run()
